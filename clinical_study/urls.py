969172838c00b69fa0340413aec2d486e24471a6
Moses Otieno <mosotieno25@gmail.com>
2025-09-29 14:02:36 +0300

    Added the project

---
+++ clinical_study/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')),  # Include core app URLs
+]
+
+# Serve static files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Custom admin site headers
+admin.site.site_header = "Clinical Study Management System"
+admin.site.site_title = "Clinical Study Admin"
+admin.site.index_title = "Study Administration"
